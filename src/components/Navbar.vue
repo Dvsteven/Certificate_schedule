4c7e62d1594a73d03e1e5eb61404f17c8f943414
Steven <stevenhenao81@gmail.com>
2024-06-05 11:14:24 -0500

    implementacion exitosa de barra lateral, sin embargo no hay responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,19 +8,6 @@
         <span> Group</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-menu offset-y>
-        <template v-slot:activator="{ on }">
-          <v-btn text v-on="on">
-            <v-icon left>expand_more</v-icon>
-            <span>Menu</span>
-          </v-btn>
-        </template>
-        <v-list flat>
-          <v-list-item v-for="link in links" :key="link.text" router :to="link.route" active-class="border">
-            <v-list-item-title>{{ link.text }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
       <v-btn text @click="goToLogin">
         <span>Exit</span>
         <v-icon right>exit_to_app</v-icon>
@@ -28,43 +15,41 @@
     </v-app-bar>
 
     <!-- Barra lateral -->
-    <v-navigation-drawer v-model="drawer" app dark class="blue darken-4" :mini-variant="miniVariant" :width="miniVariant ? '56' : '250'">
-      <v-layout column align-center>
-        <v-flex class="mt-5">
-          <v-avatar size="100">
-            <img src="../../public/user.jpg" alt="">
-          </v-avatar>
-          <p class="white--text subheading mt-1 text-center">Usuario</p>
-        </v-flex>
-        <v-flex class="mt-4 mb-4">
-          <v-btn color="primary" @click="mostrarModal = true">
-            <v-icon left>add</v-icon>
-            Nuevo Usuario
-          </v-btn>
-        </v-flex>
-      </v-layout>
-      <v-list flat>
-        <v-list-item v-for="link in links" :key="link.text" router :to="link.route" active-class="border">
-          <v-list-item-action>
-            <v-icon>{{ link.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>{{ link.text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-      <div class="linea"></div>
-      <v-list-item @click="toggleDarkMode">
-        <v-list-item-action>
-          <v-icon>brightness_3</v-icon>
-        </v-list-item-action>
-        <v-list-item-content>
-          <v-list-item-title>Modo Oscuro</v-list-item-title>
-        </v-list-item-content>
-        <v-list-item-action>
-          <v-switch v-model="darkMode" @click.stop=""></v-switch>
-        </v-list-item-action>
-      </v-list-item>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      dark
+      class="blue darken-4"
+      :mini-variant="miniVariant"
+      :clipped="$vuetify.breakpoint.smAndDown"
+      :temporary="$vuetify.breakpoint.smAndDown"
+    >
+      <v-responsive>
+        <v-layout column align-center>
+          <v-flex class="mt-5">
+            <v-avatar :size="miniVariant ? 50 : 100" class="avatar">
+              <img src="../../public/user.jpg" alt="">
+            </v-avatar>
+            <p class="white--text subheading mt-1 text-center" v-if="!miniVariant">Usuario</p>
+          </v-flex>
+          <v-flex class="mt-4 mb-4">
+            <v-btn color="primary" @click="mostrarModal = true" :class="{ 'mini-button': miniVariant }">
+              <v-icon left>add</v-icon>
+              <span v-if="!miniVariant">Nuevo Usuario</span>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+        <v-list flat>
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route" active-class="border">
+            <v-list-item-action>
+              <v-icon>{{ link.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-if="!miniVariant">{{ link.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-responsive>
     </v-navigation-drawer>
   </div>
 </template>
@@ -73,23 +58,19 @@
 export default {
   data: () => ({
     drawer: true,
-    darkMode: false,
     miniVariant: false,
+    darkMode: false,
     links: [
-          {icon: 'dashboard', text:'Panel principal', route: '/dashboard'},
-          {icon: 'event', text:'Agenda General', route: '/agenda'},
-          {icon: 'mail', text:'Enviar correo', route: '/email'},
-          {icon: 'search', text:'Visualizar vigencias', route: '/vigency'}
+      { icon: 'dashboard', text: 'Panel principal', route: '/dashboard' },
+      { icon: 'event', text: 'Agenda General', route: '/agenda' },
+      { icon: 'mail', text: 'Enviar correo', route: '/email' },
+      { icon: 'search', text: 'Visualizar vigencias', route: '/vigency' }
     ]
   }),
   methods: {
     goToLogin() {
       // Redirigir al usuario a la página de inicio de sesión
       this.$router.push({ name: 'Login' });
-    },
-    toggleDarkMode() {
-      this.darkMode = !this.darkMode;
-      document.body.classList.toggle('dark-mode', this.darkMode);
     },
     toggleDrawer() {
       // Cambiar el estado de la barra lateral entre expandida y minimizada
@@ -103,5 +84,18 @@ export default {
 .linea {
   border-top: 1px solid white;
   margin: 10px 0;
+}
+.avatar {
+  transition: width 0.3s, height 0.3s;
+}
+.mini-button {
+  padding: 0 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+}
+.mini-button v-icon {
+  margin-right: 0;
 }
 </style>
